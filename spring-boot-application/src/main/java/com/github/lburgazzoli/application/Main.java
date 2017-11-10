3aaee7155256874e77f81bcc1d0327a6186e7816
Paolo Antinori <pantinor@redhat.com>
2017-11-10 13:02:42 +0100

    example syndesis-maven-plugin

--- spring-boot-application/src/main/java/com/github/lburgazzoli/application/Main.java
+++ spring-boot-application/src/main/java/com/github/lburgazzoli/application/Main.java
@@ -4,6 +4,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import io.syndesis.integration.runtime.api.SyndesisExtensionAction;
 
 @SpringBootApplication
 public class Main {
@@ -11,6 +12,7 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    @SyndesisExtensionAction(id ="myid", value="myValue", description="myDescription", entrypoint="myEntrypoint", inputDataShape="myInputDataShape", name="myName", outputDataShape="myOutputDataShape", tags={"a","b"})
     @Component
     public static class MainRouteBuilder extends RouteBuilder {
         @Override
